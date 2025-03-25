830d554b74409171c3ea2d16379792b036732ca3
sandhikagalih <sandhikagalih@gmail.com>
2025-03-25 14:06:07 +0700

    Menambahkan latihan3.php

---
+++ kuliah/pertemuan7/latihan3.php
@@ -0,0 +1,39 @@
+<?php
+$mahasiswa = [
+  [
+    'nama' => 'Sandhika',
+    'nim' => '043040023',
+    'email' => 'sandhika@gmail.com',
+    'jurusan' => 'Teknik Informatika'
+  ],
+  [
+    'nama' => 'Doddy',
+    'nim' => '034030123',
+    'jurusan' => 'Teknik Mesin',
+    'email' => 'doddy@gmail.com',
+  ]
+];
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>GET</title>
+</head>
+
+<body>
+  <h2>Daftar Mahasiswa</h2>
+  <ul>
+    <?php foreach ($mahasiswa as $m) : ?>
+      <li>
+        <a href="latihan4.php?nama=<?= $m['nama']; ?>">
+          <?= $m['nama']; ?>
+        </a>
+      </li>
+    <?php endforeach; ?>
+  </ul>
+</body>
+
+</html>
---
+++ kuliah/pertemuan7/latihan4.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Detail Mahasiswa</title>
+</head>
+
+<body>
+  <h2>Detail Mahasiswa</h2>
+  <ul>
+    <li>Nama:</li>
+    <li>NIM:</li>
+    <li>Email:</li>
+    <li>Jurusan:</li>
+  </ul>
+</body>
+
+</html>
